2e6d2b77712b8909cef8344ac8a278a7dd398bf8
Powers <cjpowers3@gmail.com>
2017-07-09 13:07:30 -0600

    initial :rocket:

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyCguWvHIkzEErM3exXszCO5gCSpAeTEa68",
+    authDomain: "bikerack-9ead1.firebaseapp.com",
+    databaseURL: "https://bikerack-9ead1.firebaseio.com",
+    projectId: "bikerack-9ead1",
+    storageBucket: "",
+    messagingSenderId: "650001000670"
+  };
